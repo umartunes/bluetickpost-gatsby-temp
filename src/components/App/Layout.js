191c8d29f4570d8a08aa162cade4fd8db5379851
Umar Ahmad <s.umarahmad@gmail.com>
2021-03-16 16:23:15 +0500

    Tawkto Integrated

--- src/components/App/Layout.js
+++ src/components/App/Layout.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { RecoilRoot } from 'recoil'
 import GoTop from './GoTop'
 import SEO from './SEO'
+import tawkTo from "tawkto-react";
+
+// tawk.to configurations
+const tawkToPropertyId = '60508301f7ce182709308b7d'
+const tawkToKey = '1f0t7vgau'
+// https://tawk.to/chat/tawkToPropertyId/tawkToKey // Direct Chat Link
 
 const Layout = ({ children }) => {
+
+    // Inject Tawk.to if not already injected
+    useEffect(() => {
+        if(!window.injectedTawkTo){
+            window.injectedTawkTo = true
+            console.log("tawk")
+            tawkTo(tawkToPropertyId, tawkToKey)
+        }
+    }, [])
+
+
     return (
         <RecoilRoot>
             <SEO />
