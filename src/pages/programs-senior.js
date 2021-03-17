315d1ae08883ea4d580bd5f27d70b60e9615ffae
Umar Ahmad <s.umarahmad@gmail.com>
2021-03-17 18:18:40 +0500

    Senior Programs Added

---
+++ src/pages/programs-senior.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react'
+import Layout from "../components/App/Layout"
+import NavbarThree from "../components/App/NavbarThree"
+
+import Footer from "../components/App/Footer"
+import PageBanner from '../components/ProgramsJunior/PageBanner'
+import OurServices from '../components/ProgramsJunior/OurServices'
+import CourseDetailsContent from '../components/ProgramsJunior/CourseDetailsContent'
+import RelatedCourses from '../components/ProgramsJunior/RelatedCourses'
+
+const Details = () => {
+
+    const [program, setPtogram] = useState({
+        title: "Senior (Grade 6+)",
+        description: `Coding has become an essential part of every industry. Taking their programming skills to the next level with our Senior Program, will make them independent developers. By the end of the program, they’ll know how to create online games and websites. `,
+        certification: `We gamify the learning through badges to recognize the growth of our students. They will be awarded a certificate of completion after they finish every course. Students will have to submit a presentation or project, required to graduate the program. `,
+        courseFor: `Our Senior program is designed for 6 to 8 graders. 10 courses are included in this program. We also conduct exams after every course to ensure that efficient results are obtained. This program also offers leadership projects to improve the soft skills of students.`,
+        
+        price: "$84/month",
+        duration: "12 - 16 Weeks",
+        courses: "10",
+        support: "12 - 18 Weeks",
+        reports: "Quarterly",
+    })
+
+    const [curriculum, setCurriculum] = useState([
+        {
+            heading: `Introduction to Coding 201`,
+            description: `Did you learn the basics of coding and loved it? Dive deep into the world of programming. `
+        },
+        {
+            heading: `Animation  201`,
+            description: `Do you want to create animations to help your juniors understand the basic concept of your major? Our animation course can help you do that! `
+        },
+        {
+            heading: `Scratch  201`,
+            description: `Do you love designing games? Design impressive stories, animations, and games through the Scratch course.`
+        },
+        {
+            heading: `HTML 201`,
+            description: `Do you get amazed by the complex websites out there on the internet? Learn HTML to reveal the secrets behind them.`
+        },
+        {
+            heading: `Cyber Safety 201`,
+            description: `Do you want to ensure that nobody is spying on you while you are doing your work on the internet? Learn from our Cyber Safety course to ensure that.`
+        },
+        {
+            heading: `Game Design 201`,
+            description: `Do you not like any games on the play store? Do you have plenty of ideas in your brain? Game Design 101 is here to turn your dreams into reality.`
+        },
+        {
+            heading: `VR Vex 201`,
+            description: `Already know the basics of coding virtual robots? Enroll in our VR Vex course to further step up your game. `
+        },
+        {
+            heading: `TinkerCad 201`,
+            description: `Do you know the basics of 3D modeling and want to take it to the next level? Our TinkerCad course is designed to do just that.`
+        },
+        {
+            heading: `Python 201`,
+            description: `Python Turtle is known to be the easiest programming language. Learn through our Python Course and become a master. `
+        },
+        {
+            heading: `Hacking 201`,
+            description: `Did someone tell you that hacking can be used to protect one’s self from potential dangers on the internet? Learn ethical hacking to ensure your safety on the internet. 
+            `
+        },
+    ])
+
+    return (
+        <Layout>
+            <NavbarThree />
+            <PageBanner
+                pageTitle={program.title}
+                homePageText="Home"
+                homePageUrl="/"
+                activePageText="Course Details"
+            />
+            <CourseDetailsContent program={program} curriculum={curriculum} />
+            <RelatedCourses />
+            <OurServices />
+            <Footer />
+        </Layout>
+    );
+}
+
+export default Details
