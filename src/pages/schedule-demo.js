dbb14836d01035a1737fa1912d215f7a911d1087
Umar Ahmad <s.umarahmad@gmail.com>
2021-03-31 18:33:15 +0500

    schedule-demo page WIP

---
+++ src/pages/schedule-demo.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Layout from "../components/App/Layout"
+// import NavbarThree from "../components/App/NavbarThree"
+// import Footer from "../components/App/Footer"
+// import StartProject from '../components/Common/StartProject'
+// import PageBanner from '../components/Schedule/PageBanner'
+// import WhyStem1 from '../components/Schedule/WhyStem1'
+// import VirtualClass from '../components/Schedule/VirtualClass'
+
+// import { InlineWidget } from "react-calendly";
+
+const FAQ = () => {
+    return (
+        <Layout>
+            <div class="iframe-container"><iframe loading="lazy" src="https://admin-stem2.zohobookings.com/portal-embed#/customer/4165035000000024881"></iframe></div>
+            {/* <div className="container">
+            </div> */}
+        </Layout>
+    );
+}
+
+export default FAQ
