d6b9c9b741c17ca1021a5edf5b374458cc53a2f8
Umar Ahmad <s.umarahmad@gmail.com>
2021-03-31 16:39:16 +0500

    Zoho SalesIQ Inegrated

--- src/components/App/Layout.js
+++ src/components/App/Layout.js
@@ -11,13 +11,31 @@ const tawkToKey = '1f0t7vgau'
 
 const Layout = ({ children, ...otherProps }) => {
 
-    // Inject Tawk.to if not already injected
     useEffect(() => {
+        // Inject Tawk.to if not already injected
         if (!window.injectedTawkTo) {
             // console.log("tawk")
             window.injectedTawkTo = true
             tawkTo(tawkToPropertyId, tawkToKey)
         }
+        
+        // Inject Zoho SalesIQ
+        if (!window.injectedZohoIQ) {
+            window.injectedZohoIQ = true
+            window.$zoho=window.$zoho || {};
+            window.$zoho.salesiq = window.$zoho.salesiq || {widgetcode:"264574aeef8b127fb85039b1a70918612bfe049700da65b023b2667f82b39331", values:{},ready:function(){}};
+            var d=document;
+            var s=d.createElement("script");
+            s.type="text/javascript";
+            s.id="zsiqscript";
+            s.defer=true;
+            s.src="https://salesiq.zoho.com/widget";
+            var t=d.getElementsByTagName("script")[0];
+            t.parentNode.insertBefore(s,t);
+            // d.write("<div id='zsiqwidget'></div>");
+        }
+    
+
     }, [])
 
 
