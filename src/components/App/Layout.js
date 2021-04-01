e97cc0bf8acba2174a81e6511378cfcdb8b66347
Umar Ahmad <s.umarahmad@gmail.com>
2021-04-01 15:41:33 +0500

    tawk.to disabled

--- src/components/App/Layout.js
+++ src/components/App/Layout.js
@@ -2,22 +2,22 @@ import React, { useEffect } from 'react'
 import { RecoilRoot } from 'recoil'
 import GoTop from './GoTop'
 import SEO from './SEO'
-import tawkTo from "tawkto-react";
+// import tawkTo from "tawkto-react";
 
 // tawk.to configurations
-const tawkToPropertyId = '60508301f7ce182709308b7d'
-const tawkToKey = '1f0t7vgau'
+// const tawkToPropertyId = '60508301f7ce182709308b7d'
+// const tawkToKey = '1f0t7vgau'
 // https://tawk.to/chat/tawkToPropertyId/tawkToKey // Direct Chat Link
 
 const Layout = ({ children, ...otherProps }) => {
 
     useEffect(() => {
         // Inject Tawk.to if not already injected
-        if (!window.injectedTawkTo) {
-            // console.log("tawk")
-            window.injectedTawkTo = true
-            tawkTo(tawkToPropertyId, tawkToKey)
-        }
+        // if (!window.injectedTawkTo) {
+        //     // console.log("tawk")
+        //     window.injectedTawkTo = true
+        //     tawkTo(tawkToPropertyId, tawkToKey)
+        // }
         
         // Inject Zoho SalesIQ
         if (!window.injectedZohoIQ) {
