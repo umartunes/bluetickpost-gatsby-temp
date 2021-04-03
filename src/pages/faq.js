9c2f2d91d0a34ef967d5c3eb8f6470865d70e884
Chandu <chandu231@gmail.com>
2021-04-02 23:23:09 -0400

    Updated FAQ's

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -18,6 +18,22 @@ const faqs = [
         answer: `No, your child doesn’t need prior coding experience for Junior and Senior Programs. Only students who graduate from our Senior Program can enrol in STEM1’s Leader Program.`
     },
     {
+        question: `Who can participate in Jr.Hacks 2021?`,
+        answer: `Peel Students from grade 2 to 8 are eligible to participate. No prior hackathon or coding experience required.`
+    },
+    {
+        question: `Will my child have to work in groups or alone?`,
+        answer: `STEM1 is designed to foster leadership and communication skills in children. All participants will be divided into groups and guided by a skilled mentor to complete the challenge.`
+    },
+    {
+        question: `Where and when can I pickup the SWAG bag?`,
+        answer: `SWAG bags will be available for pickup from Wednesday Noon till Friday 2PM at select Meridian branches & Streetsville HomeHardware store.`
+    },
+    {
+        question: `How will you make sure my child can access the information for the event?`,
+        answer: `Students will be using our learning platform. Our Mentors will contact them before the event to make sure everything is working fine.`
+    },
+    {
         question: `If my child has finished Junior Program, do they need to learn every course in the Senior Program?`,
         answer: `No, they will be assessed for their knowledge and directly enrolled at level 6 or 7 of the Senior Program.`
     },
@@ -25,14 +41,11 @@ const faqs = [
         question: `Can my child directly enrol in the Leader Program?`,
         answer: `Only students who graduate from our Senior Program can enrol in STEM1’s Leader Program.`
     },
-    {
-        question: `Will my child have to meet his peers personally every month?`,
-        answer: `Junior and Senior Programs are conducted virtually and they don’t need to meet in-person. Students enrolled in the Leader Program should meet once a month for leadership workshops.`
-    },
+    
     {
         question: `How do you charge your fees?`,
-        answer: `STEM1 offers two payment options - monthly and yearly. Monthly costs $100, which you must pay at the beginning of every month. 
-        <br><br>Yearly fee costs $1000, which needs to be paid in advance. If you’re unsatisfied with our programs, we offer a money back guarantee.`
+        answer: `STEM1 offers two payment options - monthly and yearly. Monthly costs $120, which you must pay at the beginning of every month. 
+        <br><br>Yearly fee has a 20% discount, which needs to be paid in advance. If you’re unsatisfied with our programs, we offer a money back guarantee.`
     },
     {
         question: `What if my child misses a class?`,
