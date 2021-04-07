fca11afce503c8ee3827c042c7fdfd3a238952ec
Umar Ahmad <s.umarahmad@gmail.com>
2021-04-07 15:33:39 +0500

    Balloon Game inserted in /balloon route

---
+++ src/pages/balloon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from "../components/App/Layout"
+
+const BalloonGame = () => {
+    
+    return (
+        <Layout>
+            <div className="balloon-game-iframe-container"><iframe title=" "  src="https://balloon-popping-stem.web.app"></iframe></div>
+        </Layout>
+    );
+}
+
+export default BalloonGame
