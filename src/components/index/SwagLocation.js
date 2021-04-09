83f64e93cc021de2896856d7841c717da3378929
Chandu <chandu231@gmail.com>
2021-04-08 23:08:35 -0400

    Updated SWAG screen

--- src/components/index/SwagLocation.js
+++ src/components/index/SwagLocation.js
@@ -5,21 +5,27 @@ const swaglocation = () => {
   return (
     <div className="project-start-area bg-color py-5">
       <div className="container">
-            <div className="project-start-content text-center">
-            <h4 className="text-white">Pickup Location.</h4>
+        <div className="project-start-content text-center">
+          <h4 className="text-white">Pickup Location.</h4>
 
-            <h2 className="text-white">Streetsville Home Hardware</h2>
-            <p>3 Queen St N, Mississauga ON L5N 1A2</p>
+          <h2 className="text-white">April 8 - Rickhansen High School</h2>
+          <p>1150 Dream Crest Rd, Mississauga</p>
 
-<a href="https://goo.gl/maps/bqyyumwVNYu4UWzR7" className="default-btn" target="_blank" >Get Directions</a>
-            {/* <Link
+          <a
+            href="https://goo.gl/maps/sEDy1YJKir52sF3d9"
+            className="default-btn"
+            target="_blank"
+          >
+            RickHansen Get Directions
+          </a>
+          {/* <Link
                 to="https://goo.gl/maps/bqyyumwVNYu4UWzR7"
                 className="default-btn"
                 style={{ paddingLeft: 25 }}
             >
                 Directions <span></span>    
             </Link> */}
-            </div>
+        </div>
       </div>
     </div>
   );
