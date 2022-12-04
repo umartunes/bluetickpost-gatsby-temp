3adbd37648456cbc990aac05229cc535d7ffce67
Umar <hello@umarahmad.me>
2022-12-04 22:20:06 +0500

    Old Registration form implemented using Iframe

--- src/pages/apply.js
+++ src/pages/apply.js
@@ -2,12 +2,38 @@ import React from 'react'
 import Layout from "../components/App/Layout"
 import NavbarTwo from "../components/App/NavbarTwo";
 import Footer from "../components/App/Footer"
-import RegisterForm from '../components/Register/RegisterForm'
+// import RegisterForm from '../components/Register/RegisterForm'
+
+import starIcon from '../assets/images/star-icon.png'
+
 const Contact = () => {
     return (
         <Layout>
             <NavbarTwo />
-            <RegisterForm />
+            {/* <RegisterForm /> */}
+            <section className="contact-area ptb-100 bg-fafafb">
+                <div className="container">
+                    <div className="section-title mb-0 pb-0">
+                        <span className="sub-title">
+                            <img src={starIcon} alt="contact" />
+                            Application Form
+                        </span>
+                        <h2>Join The Lead Tech Industry</h2>
+                        <p>Please fill in the form below to apply for a course. All fields are required.</p>
+                    </div>
+
+                    <div style={{ minHeight: 400 }} className="d-flex justify-content-center align-items-center">
+                        <div className="container">
+                            <div className="demo-iframe-container mt-5" style={{ height: 1200 }}>
+                                <iframe title="" loading="lazy" src="https://techna.web.app/apply"></iframe>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
+
             <Footer />
         </Layout>
     );
