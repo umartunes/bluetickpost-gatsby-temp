6d54a4a714af8dc85376086b8c5e636283601e70
Umar <hello@umarahmad.me>
2022-12-07 20:26:56 +0500

    Apply Form Iframe size enhanced

--- src/pages/apply.js
+++ src/pages/apply.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Layout from "../components/App/Layout"
 import NavbarTwo from "../components/App/NavbarTwo";
 import Footer from "../components/App/Footer"
@@ -7,11 +7,23 @@ import Footer from "../components/App/Footer"
 import starIcon from '../assets/images/star-icon.png'
 
 const Contact = () => {
+
+    useEffect(() => {
+
+        document.body.style.overflowY = 'hidden'
+        document.body.style.backgroundColor = '#eeeeee'
+
+        return () => {
+            document.body.style.overflowY = 'auto'
+        };
+
+    }, []);
+
     return (
         <Layout>
             <NavbarTwo />
             {/* <RegisterForm /> */}
-            <section className="contact-area ptb-100" style={{backgroundColor: '#eeeeee'}}>
+            {/* <section className="contact-area" style={{ backgroundColor: '#eeeeee' }}>
                 <div className="container-fluid">
                     <div className="section-title mb-0 pb-0">
                         <span className="sub-title">
@@ -24,16 +36,25 @@ const Contact = () => {
 
                     <div style={{ minHeight: 500 }} className="d-flex justify-content-center align-items-center">
                         <div className="container" style={{ padding: 0 }}>
-                            <div className="demo-iframe-container mt-3" style={{ height: '100vh', paddingTop: '100vh' }}>
+                            <div className="demo-iframe-container mt-0" style={{ height: '100vh', paddingTop: '100vh' }}>
                                 <iframe title="" loading="lazy" src="https://techna.web.app/apply"></iframe>
                             </div>
                         </div>
-                    </div>
+                    </div> 
 
                 </div>
-            </section>
+            </section> */}
 
-            <Footer />
+            <iframe style={{
+                display: 'block',
+                background: "#000",
+                border: "none",         
+                height: "calc( 100vh - 95px )",  
+                width: "100%",
+                maxWidth: '100vw'
+            }} title="" loading="lazy" src="https://techna.web.app/apply"></iframe>
+
+            {/* <Footer /> */}
         </Layout>
     );
 }
