78b7379956871ed89950c85a4ced56a6546dbe42
Umar <hello@umarahmad.me>
2023-03-05 12:39:44 +0500

    Fee Details Added to the web and mobile application development course

--- src/pages/courses/web-and-mobile-app-development.js
+++ src/pages/courses/web-and-mobile-app-development.js
@@ -16,7 +16,7 @@ const program = {
   certification: `We gamify learning through awarding points, badges and certificates to recognize the progress & hardwork of our students. Students will be awarded a certificate of completion after they finish the course. Students are required to submit assignments and projects assigned to them to graduate this course.`,
   courseFor: `Our Web & Mobile App Development program is designed for students who have passed grade 10 from their schools. and are motivated to do achieve big things in life.`,
 
-  price: "Rs. 2500",
+  price: "Rs. 2500 / month",
   discountPercentage: "50% Off",
   priceBeforeDiscount: "Rs. 5000",
   courseDuration: "~6 months",
