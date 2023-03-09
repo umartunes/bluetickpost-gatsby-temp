8f99b39dc4a805aa8c43ea4e35af787cf92b23c6
Umar <hello@umarahmad.me>
2023-03-09 11:58:52 +0500

    Welcome Message updated

--- src/components/Jobs/JobApplicationForm.js
+++ src/components/Jobs/JobApplicationForm.js
@@ -284,7 +284,7 @@ const JobApplicationForm = () => {
                                 Job Application Form
                             </span>
                             <h2>Join The Lead Tech Industry</h2>
-                            <p className='mx-auto' style={{ maxWidth: '80%' }}>Thank you for your interest in joining our team! Please complete the form below to apply for one of many great opportunities with Techna - and someone will reach out to you if we think it's a good fit!</p>
+                            <p className='mx-auto' style={{ maxWidth: '80%' }}>Please complete the form below to apply for one of many great opportunities with Techna - and someone will reach out to you if we think it's a good fit!</p>
                         </div>
 
                         <div className="row">
