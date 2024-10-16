eed07a65325fd3576a5a5191fcbe45f2fb7d0248
umar@ubuntu <s.umarahmad@gmail.com>
2024-10-16 15:38:44 +0500

    Deploy script renamed

--- deploy.sh
+++ deploy.sh
@@ -19,18 +19,22 @@ log_info "Ensure that Node.js version 12.12 is installed. To install, run: (nvm 
 log_info "Node.js version 12.12 must be selected manually before this script can be run."
 log_info "Select Node.js version 12.12 using: (nvm use 12.12)."
 
-# Build the project
+# Step 1: Build the project
+log_info "✔ Starting project build..."
 npm run build
 log_info "✔ Project build completed successfully."
 
-# Build the Docker image
+# Step 2: Build the Docker image
+log_info "✔ Building Docker image 'bluetickpost-static-site'..."
 docker build -t bluetickpost-static-site .
 log_info "✔ Docker image 'bluetickpost-static-site' built successfully."
 
-# Tag the Docker image
+# Step 3: Tag the Docker image
+log_info "✔ Tagging Docker image 'umartunes/techna-tools:bluetickpost-static-site'..."
 docker tag bluetickpost-static-site umartunes/techna-tools:bluetickpost-static-site
 log_info "✔ Docker image tagged as 'umartunes/techna-tools:bluetickpost-static-site'."
 
-# Push the Docker image to the repository
+# Step 4: Push the Docker image to the repository
+log_info "✔ Pushing Docker image to Docker Hub: 'umartunes/techna-tools:bluetickpost-static-site'..."
 docker push umartunes/techna-tools:bluetickpost-static-site
 log_info "✔ Docker image pushed to 'umartunes/techna-tools:bluetickpost-static-site' successfully."
